f41b11df4696658ba7c8e1b270ca2433f3964ec8
asakalouski <asakalouski@172.16.0.223>
2017-11-14 17:49:37 +0300

    first commit

---
+++ src/test/java/PatientPortal/APITesting/AppTest.java
@@ -0,0 +1,19 @@
+package PatientPortal.APITesting;
+
+import static io.restassured.RestAssured.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	@Test
+	public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
+	        
+	    given().
+	    when().
+	        get("http://ergast.com/api/f1/2017/circuits.json").
+	    then().
+	        assertThat().
+	        statusCode(200);
+	}
+}
