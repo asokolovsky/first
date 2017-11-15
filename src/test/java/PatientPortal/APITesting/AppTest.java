222fd5dba460e4f3d5b386ba2d67aa49b306ecd1
asakalouski <asakalouski@172.16.0.223>
2017-11-15 10:45:53 +0300

    some changes

--- src/test/java/PatientPortal/APITesting/AppTest.java
+++ src/test/java/PatientPortal/APITesting/AppTest.java
@@ -1,10 +1,11 @@
 package PatientPortal.APITesting;
 
 import static io.restassured.RestAssured.*;
-
 import org.junit.Test;
+import junit.framework.TestCase;
 
 public class AppTest 
+extends TestCase
 {
 	@Test
 	public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
